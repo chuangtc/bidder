39ccbee5511308147bdbd445c31158b654d3b7f1
Jason Chuang <chuangtcee@gmail.com>
2021-04-24 15:32:13 +0800

    parse protobuf

--- bw_dsp/views.py
+++ bw_dsp/views.py
@@ -14,8 +14,21 @@ def home_view(*args, **kwargs):
 
 def bidding_strategy_view(request, *args, **kwargs):
     bid_request = request.body
-    bid_floor = 10
-    # new_b = pickle.loads(bid_request)
+    #print(bid_request)
+    bid_request_str=bid_request.decode('utf-8')
+    string_id_beg = bid_request_str.index("string id")
+    string_id = bid_request_str[string_id_beg:string_id_beg + 20]
+    string_id = (string_id[string_id.index("=")+1:string_id.index(";")])
+
+    repeated_Imp_imp_beg = bid_request_str.index("repeated Imp imp")
+    repeated_Imp_imp = bid_request_str[repeated_Imp_imp_beg:repeated_Imp_imp_beg + 25]
+    repeated_Imp_imp = (repeated_Imp_imp[repeated_Imp_imp.index("=")+1:repeated_Imp_imp.index(";")])
+
+    double_bidfloor_beg = bid_request_str.index("double bidfloor")
+    double_bidfloor = bid_request_str[double_bidfloor_beg:double_bidfloor_beg + 25]
+    double_bidfloor = (double_bidfloor[double_bidfloor.index("=")+1:double_bidfloor.index(";")])
+
+    bid_floor = float(double_bidfloor)
 
     queryset = ad_settings.objects.all()
     max_creative_id = -1
@@ -36,5 +49,15 @@ def bidding_strategy_view(request, *args, **kwargs):
         response.status_code = 204
         return response
 
-    obj = { "creative_id": max_creative_id, "bid_price": max_bid_price }
-    return HttpResponse(json.dumps(obj), content_type="application/json")
+    message_BidResponse = ('message BidResponse { \n',
+                           'string id = '+ string_id+';\n' ,
+                           'repeated Bid bid = '+repeated_Imp_imp+';\n',
+                           'message Bid {\n',
+                           'string id = '+ string_id+';\n',
+                           'string impid = '+str(max_creative_id)+';\n',
+                           'double price = '+str(max_bid_price)+';\n',
+                           '}\n',
+                           '}\n'
+                           )
+    #obj = { "creative_id": max_creative_id, "bid_price": max_bid_price }
+    return HttpResponse(message_BidResponse, content_type="text/plain")
